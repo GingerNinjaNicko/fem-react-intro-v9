a04f9d63056b3ce2d222e4b91ca555c6cd35a02f
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2024-12-16 14:13:45 +0000

    ✨ complete Raw React Pizza component

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,8 +1,27 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+}
+
 const App = () => {
   return React.createElement(
     "div",
     {},
-    React.createElement("h1", {}, "Padre Gino's")
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Tasty tasty"
+    }),
+    React.createElement(Pizza, {
+      name: "Americano",
+      description: "Freedon!!!"
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian",
+      description: "Yum Pinapple"
+    }),
   );
 };
 
