31169793f2d9d784b4c69b4d76201ecf5901c9f5
Nicko J. Ruddock <nick.ruddock@taxassist.co.uk>
2024-12-16 16:52:34 +0000

    `npm init` & set up prettier

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -3,7 +3,7 @@ const Pizza = (props) => {
     React.createElement("h1", {}, props.name),
     React.createElement("p", {}, props.description),
   ]);
-}
+};
 
 const App = () => {
   return React.createElement(
@@ -12,19 +12,19 @@ const App = () => {
     React.createElement("h1", {}, "Padre Gino's"),
     React.createElement(Pizza, {
       name: "The Pepperoni Pizza",
-      description: "Tasty tasty"
+      description: "Tasty tasty",
     }),
     React.createElement(Pizza, {
       name: "Americano",
-      description: "Freedon!!!"
+      description: "Freedon!!!",
     }),
     React.createElement(Pizza, {
       name: "The Hawaiian",
-      description: "Yum Pinapple"
+      description: "Yum Pinapple",
     }),
   );
 };
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
